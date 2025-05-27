4ac5376a9c79c15891858ebeb66bf70b6aa72b64
JulianLopez711 <lopezito02@gmail.com>
2025-05-27 18:01:07 -0500

    Cambios en manifestt.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icons/LogoX192.png',
+            src: '/icons/Logo192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/LogoX512.png',
+            src: '/icons/Logo512.png',
             sizes: '512x512',
             type: 'image/png'
           }
