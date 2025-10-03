2f666eba0619c45ff792d8d162eb835ac8a9f409
JulianLopez711 <lopezito02@gmail.com>
2025-10-03 14:54:27 -0500

    merge rama oscar

--- frontend/src/pages/master/CarrierManagement.tsx
+++ frontend/src/pages/master/CarrierManagement.tsx
@@ -152,7 +152,7 @@ export default function CarrierManagement() {
         params.append("page", page.toString());
         params.append("page_size", pageSize.toString());
 
-        const url = `http://127.0.0.1:8000/master/carriers/guias${
+        const url = `https://api.x-cargo.co/master/carriers/guias${
           params.toString() ? "?" + params.toString() : ""
         }`;
 
@@ -257,7 +257,7 @@ export default function CarrierManagement() {
         if (filtros.estadoPago)
           params.append("estado_pago", filtros.estadoPago);
 
-        const url = `http://127.0.0.1:8000/master/carriers/export?${params.toString()}`;
+        const url = `https://api.x-cargo.co/master/carriers/export?${params.toString()}`;
         console.log("📡 URL de exportación:", url);
 
         const response = await fetch(url, {
@@ -411,7 +411,7 @@ export default function CarrierManagement() {
         params.append("page", "1");
         params.append("page_size", "100");
 
-        const url = `http://127.0.0.1:8000/master/carriers/guias?${params.toString()}`;
+        const url = `https://api.x-cargo.co/master/carriers/guias?${params.toString()}`;
 
         console.log("🔍 Carga inicial - Request a:", url);
 
